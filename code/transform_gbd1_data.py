9347a7f08eac56c1b4d374819db815959e1f0bdb
Tobias <tobiasalsgaard@gmail.com>
2024-07-29 19:07:57 +0200

    Local Change

--- code/transform_gbd1_data.py
+++ code/transform_gbd1_data.py
@@ -2,7 +2,7 @@ import numpy as np
 from utils import import_base, df_import, create_xy
 import os
 
-df = df_import("../data/gbd1/gbd1_data.xlsx")
+df = df_import("../data/gbd1/gbd1_data.xlsx", 5000)
 base = import_base()
 mutants, fitness = create_xy(df, base)
 
